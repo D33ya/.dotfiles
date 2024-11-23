3b0abcd4b3a4c5f50daed5880fc423d72dd1076e
D33ya <D33ya@proton.me>
2024-11-23 15:12:42 -0600

    telescope

--- nvim/nvim/lua/plugins/telescope.lua
+++ nvim/nvim/lua/plugins/telescope.lua
@@ -32,11 +32,7 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+
         pickers = {
           find_files = {
             hidden = false,
