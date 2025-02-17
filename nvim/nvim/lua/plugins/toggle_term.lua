f8c02f4b07cf2f047da3d891c4f69d9638effab9
D33ya <D33ya@proton.me>
2025-02-17 14:55:19 -0600

    update toggleterm

--- nvim/nvim/lua/plugins/toggle_term.lua
+++ nvim/nvim/lua/plugins/toggle_term.lua
@@ -2,23 +2,10 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = '*',
-    config = function()
-      require('toggleterm').setup {
-        open_mapping = [[<c-\>]],
-        persist_mode = false,
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-        },
-      }
-      local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit = Terminal:new { cmd = 'lazygit', hidden = true }
-
-      function _lazygit_toggle()
-        lazygit:toggle()
-      end
-
-      vim.api.nvim_set_keymap('n', '<C-g>', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
-    end,
+    opts = {
+      open_mapping = [[<c-\>]],
+      persist_mode = false,
+      direction = 'vertical',
+    },
   },
 }
