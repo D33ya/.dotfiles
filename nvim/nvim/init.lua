56bb4305af1904f1c10573cc9d6007305df2b1bf
D33ya <D33ya@proton.me>
2025-06-03 12:19:55 -0500

    Blank Slate

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,27 +1,72 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ';'
+-- See `:help vim.opt`
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
 
--- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = true
+vim.opt.number = true
 
--- [[ Setting ]]
+vim.opt.relativenumber = true
 
-require 'settings'
+-- vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
 
--- [[ Basic Keymaps ]]
---
-require 'keymaps'
+vim.opt.conceallevel = 3
+vim.opt.concealcursor = 'nc'
 
--- [[ Basic Autocommands ]]
+vim.opt.foldlevel = 1
 
-require 'autocmds'
+vim.opt.expandtab = true
 
--- [[ Install `lazy.nvim` plugin manager ]]
+vim.opt.tabstop = 4
 
-require 'config.lazy'
+vim.opt.shiftwidth = 4
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+vim.opt.virtualedit = 'block'
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 500
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 999
